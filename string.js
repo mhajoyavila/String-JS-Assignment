444eea2ed0bedc7ba01507d5691267cf310a6d8a
Mary Joy Avila <maryjoyavila524@gmail.com>
2023-09-05 11:07:51 +0800

    first commit

---
+++ string.js
@@ -0,0 +1,2 @@
+const myTextArea = prompt ("Good day! How may I help you? (please limit in 150 characters)");
+alert ("You have written " + myTextArea.length +" characters," + " and you only have " + (150-myTextArea.length) + " remaining characters!");
